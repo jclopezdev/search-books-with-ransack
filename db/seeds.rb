77a7eafd888239a2ff2da4d386bb3a97c4e20d31
Juan Carlos López <jclopezdev@gmail.com>
2019-02-21 16:42:08 -0600

    Use faker to seed books table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+100.times do
+  Book.create(
+    isbn: Faker::Code.isbn,
+    name: Faker::Book.title,
+    author: Faker::Book.author,
+    year: Faker::Number.between(1900, 2019),
+    genre: Faker::Number.between(1, 5),
+    price: Faker::Commerce.price
+  )
+end
