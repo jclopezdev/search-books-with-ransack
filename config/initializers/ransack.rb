e896f83ed837b67851642ee8e8108906797ad65e
Juan Carlos López <jclopezdev@gmail.com>
2019-02-25 23:05:14 -0600

    Create ransack initializer

---
+++ config/initializers/ransack.rb
@@ -0,0 +1,7 @@
+Ransack.configure do |c|
+  c.custom_arrows = {
+    up_arrow: '<i class="custom-up-arrow-icon"></i>',
+    down_arrow: 'U+02193',
+    default_arrow: '<i class="default-arrow-icon"></i>'
+  }
+end
